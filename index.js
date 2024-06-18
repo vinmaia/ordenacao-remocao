8083f6c1127d409f995e66f96d9895dff8612e44
Maia <vincecake008@gmail.com>
2024-06-18 08:41:28 -0300

    feat: ordenado array

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let numeros = [4, 3, 1, 5, 5, 7, 2, -47, 2001];
 
-for(let i = 0; i < numeros.length; i++) {
-    for(let j = 0; j < numeros.length; j++) {
-        if(numeros[i] > numeros[j]){
-            
-        }
+for (let i = 0; i < numeros.length; i++) {
+  for (let j = 0; j < numeros.length; j++) {
+    if (numeros[i] > numeros[j]) {
+      numeros[i] = numeros[j];
+      numeros[j] = aux;
     }
-}
+  }
+};
+console.log(numeros);
