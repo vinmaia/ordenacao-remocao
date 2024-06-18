6e1c3781d1ee9d4f6c25e4b125cb60fa0b5b3219
Maia <vincecake008@gmail.com>
2024-06-18 08:41:08 -0300

    feat: ordenado array

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 let numeros = [4, 3, 1, 5, 5, 7, 2, -47, 2001];
+
+for(let i = 0; i < numeros.length; i++) {
+    for(let j = 0; j < numeros.length; j++) {
+        if(numeros[i] > numeros[j]){
+            
+        }
+    }
+}
