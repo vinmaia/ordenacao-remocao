edc74d327cbd606ef596eb994b91d82efb8baf8b
Maia <vincecake008@gmail.com>
2024-06-18 08:51:08 -0300

    refactor: adiciona logica de ordenacao e remocao literal do array

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let numeros = [4, 3, 1, 5, 5, 7, 2, -47, 2001];
 
 for (let i = 0; i < numeros.length; i++) {
-  for (let j = 0; j < numeros.length; j++) {
+  for (let j = i + 1; j < numeros.length; j++) {
     if (numeros[i] > numeros[j]) {
+      let aux = numeros[i];
       numeros[i] = numeros[j];
       numeros[j] = aux;
-    }
+    } else if (numeros[i] == numeros[j]) {
+        numeros.splice(j, 1);
+        j--;
+      }
   }
-};
+}
 console.log(numeros);
